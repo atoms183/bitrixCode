8f42977987c29ad6bec51b54c12363b3d161b706
Дмитрий Глазвин <sony183@bk.ru>
2017-01-04 11:24:51 +0300

    commit

--- template.php
+++ template.php
@@ -17,54 +17,52 @@ $this->setFrameMode(true);
    <div class="block_content_right" >	
 	                             
 	 <div class="video_content" >
-		
+	 
+
 	   <div class="video_top" >
 										 
         <img src="<?=SITE_TEMPLATE_PATH?>/images/video/video_1.jpg" />
 		<h3><a href="/">Дизайн ногтей с использованием гель-лака «Кошачий глаз», Витражного гель-лака и акриловой краски</a></h3>
-		<p>Магнитные гель-лаки всегда весьма эффектно выглядят на ногтях. Давайте вместе рассмотрим все этапы создания маникюра с использованием нескольких гель-лаков и акриловой краски. И результат - один из вариантов неповторимого дизайна ногтей.</p>
+		<p>Магнитные гель-лаки всегда весьма эффектно выглядят на ногтях. Давайте вместе рассмотрим все этапы создания маникюра 
+		с использованием нескольких гель-лаков и акриловой краски. И результат - один из вариантов неповторимого дизайна ногтей.</p>
 			<span class="news_date">16 октября 2016</span>
-			</div><!-- .video_top---->	
+			</div><!--video_top-->	
 
 			<div class="video_bott" >
 
 	<?foreach($arResult["ITEMS"] as $arItem):?>
 
 
-						<?
-						$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-						$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-						?>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+			?>
+			<div class="video_bott_text" style="padding-right: 30px;" id="<?=$this->GetEditAreaId($arItem['ID']);?>">	
 
-	                   <div class="video_bott_text" id="<?=$this->GetEditAreaId($arItem['ID']);?>">	
+			<a href="#win<?=$this->GetEditAreaId($arItem['ID']);?>"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"/></a><!-- Вывод превью картинки -->
+			<!-- Модальное окно -->
 
-
-     <a href="#win<?=$this->GetEditAreaId($arItem['ID']);?>"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"/></a><!-- Вывод превью картинки -->
-					<!-- Модальное окно -->
-
-							<a href="#x" class="overlay" id="win<?=$this->GetEditAreaId($arItem['ID']);?>"></a>
-							<div class="popup">
-								<div class="video">
-				
-								 <iframe width="853" height="480" src="<?echo $arItem["PROPERTIES"]["YOUTUBE"]["VALUE"];?>" frameborder="0" allowfullscreen></iframe> <!--Вывод видео с youtube-->
-								</div>
+				<a href="#x" class="overlay" id="win<?=$this->GetEditAreaId($arItem['ID']);?>"></a>
+					<div class="popup">
+						<div class="video">
+							<iframe width="853" height="480" src="<?echo $arItem["PROPERTIES"]["YOUTUBE"]["VALUE"];?>" frameborder="0" allowfullscreen></iframe> <!--Вывод видео с youtube-->
+							</div>
 								<a class="close" title="Закрыть" href="#x"></a>
 							</div> 
-					</a> 
+				</a> 
 		
 					<h3><a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><?echo $arItem["NAME"]?></b></a><br /></h3><!--Вывод заголовка видео-->
 					 <span class="news_date"><? echo $arItem["TIMESTAMP_X"]?></span>
-					</div><!-- .video_bott_text---->
+					</div><!-- .video_bott_text-->
 					
 	<?endforeach;?>
-	</div><!-- .video_bott---->	
+	</div><!--video_bott-->	
 		</div><!-- .kompani_block_content -->
 		
-									<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
 <br /><?=$arResult["NAV_STRING"]?>
 <?endif;?>
-		
-									 	
+						 	
 	 </div><!-- .block_content_right -->
 
 
